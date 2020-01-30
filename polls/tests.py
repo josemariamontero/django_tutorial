03250061c01428bddb37664d1ebae2aeb0a6967d
José María Montero Romero <josemaria@daryl>
2020-01-30 14:02:16 +0100

    resuelto

--- polls/tests.py
+++ polls/tests.py
@@ -23,9 +23,9 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns False for questions whose pub_date
         is older than 1 day.
         """
-        time = timezone.now() - datetime.timedelta(days=15, seconds=3)
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), True)
+        self.assertIs(old_question.was_published_recently(), False)
     
     def test_was_published_recently_with_recent_question(self):
         """
